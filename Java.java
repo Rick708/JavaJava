211cc8cf431680c6a248fb9e9163013708f9e14c
Rick708 <Rick708@users.noreply.github.com>
2020-06-06 05:56:32 +0900

    Java 変数

--- Java.java
+++ Java.java
@@ -1,5 +1,8 @@
-public class Java{
+public class Java {
   public static void main (String[] args){
-    System.out.println("Hello World");
+    int num = 10;
+    System.out.println(num);
+
+    
   }
 }

ec0b40a94543322059123fdbf4cb5720aa514750
Rick708 <Rick708@users.noreply.github.com>
2020-06-09 05:38:13 +0900

    while

--- Java.java
+++ Java.java
@@ -1,10 +1,9 @@
 public class Java {
   public static void main(String[] args) {
-    for (int j = 0; j < 3; j++){
-      for (int i = 0; i < j; i++){
-        System.out.print("*");
-      }
-      System.out.println();
+    int i = 0;
+    while (i < 2) {
+      System.out.println("i = " + i );
+      i++ ;
     } 
   }
 }

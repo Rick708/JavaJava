43e9961e62135f14d6ea5f8f98217d70a47dddbe
Rick708 <Rick708@users.noreply.github.com>
2020-06-09 05:28:16 +0900

    わからなかった問題

--- Java.java
+++ Java.java
@@ -1,6 +1,10 @@
 public class Java {
   public static void main(String[] args) {
-    for (int i = 0; i < 3; i++)
-      System.out.print("1");
+    for (int j = 0; j < 3; j++){
+      for (int i = 0; i < j; i++){
+        System.out.print("*");
+      }
+      System.out.println();
+    } 
   }
 }

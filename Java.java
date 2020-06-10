647187ee583bc0c967a7735c6dfac88d8b958490
Rick708 <Rick708@users.noreply.github.com>
2020-06-11 06:30:57 +0900

    break

--- Java.java
+++ Java.java
@@ -1,10 +1,13 @@
 public class Java {
   public static void main(String[] args) {
-    for (int i = 1; i<5; i++){
-      System.out.println(i);
-      if(i ==3){
-        break;
+    for (int i = 0; i<5; i++){
+      for (int j = 0; j<5; j++){
+        System.out.print("*");
+        if(j == 3){
+          break;
+        }
       }
+      System.out.println();
     } 
   }
 }

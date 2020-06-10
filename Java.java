4f495755c2de517fb309b1c4b13213355ce4b2f4
Rick708 <Rick708@users.noreply.github.com>
2020-06-11 06:35:54 +0900

    continue

--- Java.java
+++ Java.java
@@ -1,13 +1,13 @@
 public class Java {
   public static void main(String[] args) {
-    for (int i = 0; i<5; i++){
-      for (int j = 0; j<5; j++){
-        System.out.print("*");
-        if(j == 3){
-          break;
+    for (int i = 0; i<10; i++){
+      // for (int j = 0; j<5; j++){
+      System.out.print(i);
+        if(8<i){
+          continue;
         }
-      }
-      System.out.println();
+      // }
+      System.out.print(",");
     } 
   }
 }
